b1641f78e1b04de6231c82cc1ab3ed8d0d1f2c97
rahul0singh0 <thesincererahul@gmail.com>
2024-02-26 12:15:41 +0530

    learning about type conversion or type casting

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+// let score = null;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// // typecasting or type conversion
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); 
+// //0 => score = null, NaN => score = "abc" but 123 => score = "123"
+// // 1/0 => score = true/false
+
+// let isLoggedIn = "";
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn); // true -> for all numbers except 0
+// // false -> "", true -> "some"
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
