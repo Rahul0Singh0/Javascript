714a24689020369cbf3a3b1659cf5250c5fde5ae
rahul0singh0 <thesincererahul@gmail.com>
2024-02-27 00:20:17 +0530

    Updated

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -15,7 +15,42 @@
 // console.log(booleanIsLoggedIn); // true -> for all numbers except 0
 // // false -> "", true -> "some"
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+// ***************** Operations ******************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2); // **In cryptography, and in many more algorithm
+
+let str1 = "hello";
+let str2 = "Rahul";
+
+let str3 = str1 + str2;
+// console.log(str3); // concenate
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 4 * 5 % 3); 
+
+// Conversion
+console.log(+true); //1
+// console.log(true+); // SyntaxError
+console.log(+""); //0
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+console.log(num1);
