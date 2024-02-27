48aafb77e19bf82436fc1e65886a8a919391195d
rahul0singh0 <thesincererahul@gmail.com>
2024-02-27 16:45:48 +0530

    Stack and Heap

--- 01_basics/05_dataTypes_summary.js
+++ 01_basics/05_dataTypes_summary.js
@@ -12,41 +12,42 @@
 //    Number, Boolean, null, undefined, Symbol(unique), BigInt(Some Scientific values or larger integer values)
 
 // Note: JS is Dynamically Typed Language because don't require to tell to JS which types of value can store u=in variable
-const score = false; // JS
-// Where as TypeScript is Statically Typed Language
-// const score:number = 100; // TS
-const scoreValue = 100.3;
-const isLoggedIn = false;
-const outsideTemp = null; // empty
-let userEmail;
+// const score = false; // JS
+// // Where as TypeScript is Statically Typed Language
+// // const score:number = 100; // TS
+// const scoreValue = 100.3;
+// const isLoggedIn = false;
+// const outsideTemp = null; // empty
+// let userEmail;
 
-// same value pass but return different or unique value
-const id = Symbol('123'); // unique returnType is also Symbol
-const anotherId = Symbol('123'); // differ value return 
-console.log(id === anotherId); // false
+// // same value pass but return different or unique value
+// const id = Symbol('123'); // unique returnType is also Symbol
+// const anotherId = Symbol('123'); // differ value return 
+// console.log(id === anotherId); // false
 
-const bigNumber = 3740349852848349843848934983489n; // at last n represent to make BigInt
-console.log(typeof(bigNumber)); // bigint 
-// console.log(bigNumber);
+// const bigNumber = 3740349852848349843848934983489n; // at last n represent to make BigInt
+// console.log(typeof(bigNumber)); // bigint 
+// // console.log(bigNumber);
 
-// Reference Type (Non-Primitive) in JS
-// Types: Array, Objects, Functions
+// // Reference Type (Non-Primitive) in JS
+// // Types: Array, Objects, Functions
 
-const heros = ["shaktiman","naagraj","doga"]; // Array 
-// Object pair(key:value) form
-let myObj = {
-    name: "hitesh",
-    age: 22
-}
-// 1st way of Function declaration like variable
-const myFunction = function() {
-    console.log("Hello World");
-}
-console.log(typeof heros); // object
-console.log(typeof myObj); // object
-console.log(typeof myFunction); // function
-console.log(typeof null); // object
-console.log(typeof true); // boolean
-console.log(typeof undefined); // undefined
+// const heros = ["shaktiman","naagraj","doga"]; // Array 
+// // Object pair(key:value) form
+// let myObj = {
+//     name: "hitesh",
+//     age: 22
+// }
+// // 1st way of Function declaration like variable
+// const myFunction = function() {
+//     console.log("Hello World");
+// }
+// console.log(typeof heros); // object
+// console.log(typeof myObj); // object
+// console.log(typeof myFunction); // function
+// console.log(typeof null); // object
+// console.log(typeof true); // boolean
+// console.log(typeof undefined); // undefined
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
