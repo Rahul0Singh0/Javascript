fac8c634643bb8b0678c58ebfaf50be518f00ca6
rahul0singh0 <thesincererahul@gmail.com>
2024-03-04 09:20:01 +0530

    Updated Control statement file

--- 04_control_flow/switch.js
+++ 04_control_flow/switch.js
@@ -7,22 +7,22 @@
 //         break;
 // }
 
-const month = 3
-switch(month) {
-    case 1: 
-        console.log("January");
-        break;
-    case 2: 
-        console.log("Febraury");
-        break;
-    case 3: 
-        console.log("March");
-        break;
-    case 4: 
-        console.log("April");
-        break;
-    default: 
-        console.log("Default case match");
-        break;
-    // once case will match then after all case donot match 
-}
+// const month = 3
+// switch(month) {
+//     case 1: 
+//         console.log("January");
+//         break;
+//     case 2: 
+//         console.log("Febraury");
+//         break;
+//     case 3: 
+//         console.log("March");
+//         break;
+//     case 4: 
+//         console.log("April");
+//         break;
+//     default: 
+//         console.log("Default case match");
+//         break;
+//     // once case will match then after all case donot match 
+// }
