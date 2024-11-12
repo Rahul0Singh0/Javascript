47b5fae2e36ba83b4f396f2ecf42f5fb0a51e509
Rahul Singh <thesincererahul@gmail.com>
2024-11-12 22:52:21 +0530

    comparison operator

---
+++ Javascript/2_Operators/comparisonOperators.js
@@ -0,0 +1,19 @@
+// It will help to compare between two objects or values
+// It returns always boolean value
+console.log(10 < 20);
+console.log(8 >= 10);
+
+
+// abstract equality operator
+console.log(10 == 10);
+console.log("10" == 10); // number 10 automatically change the type into string then compare this is called coercion rule
+
+// strict equality operator
+console.log(10 === 10); // true
+console.log("10" === 10); // false
+
+// abstract not equal(!=)
+console.log("10" != 10); // false
+
+// strict not equal(!==)
+console.log("10" !== 10); // true
