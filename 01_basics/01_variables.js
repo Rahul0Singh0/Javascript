75ca1d8492f407ef50d83ea5bf2dde4a8ddb7461
Rahul Singh <thesincererahul@gmail.com>
2024-11-12 16:01:06 +0530

    update

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -22,7 +22,7 @@ console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
 // All of above analyzing by node js
 // V8 engine before node js, provides in Google chrome for executing js code at client side only
 
-// let keyword introduce in 2015 with EC6, before introduce, it has problem
+// let keyword introduce in 2015 with ES6, before introduce, it has problem
 // js does not know about scope, and doest not work based on scope
 /*
   Prefer not to use var 
