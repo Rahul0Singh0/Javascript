3e10de00183a2885b422c2bdba0335c822271f73
Rahul Singh <thesincererahul@gmail.com>
2024-11-12 17:00:51 +0530

    Assignment Operator

---
+++ Javascript/2_Operators/assignmentOperator.js
@@ -0,0 +1,14 @@
+let x = 10;
+let y = x + 5;
+x = y;
+console.log(y);
+
+let a = 10;
+a = a + 5;  // Associative R to L
+console.log(a);
+a += 5;
+console.log(a);
+
+let b = 2;
+b **= 2;
+console.log(b);
