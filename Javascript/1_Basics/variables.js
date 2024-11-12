70c97853c3c9cd5d792e22c80c8e49971e1abe1d
Rahul Singh <thesincererahul@gmail.com>
2024-11-12 16:21:12 +0530

    JS Variables

---
+++ Javascript/1_Basics/variables.js
@@ -0,0 +1,11 @@
+let a = 10;
+var b = 23;
+const c = 348;
+console.log(a);
+
+// Variable naming rule
+// Contains all english alphabets, digits, _, $
+let first_name = "rahul";
+// let first-name = "rohan"; // SyntaxError: Unexpected token '-'
+let first$name;
+// let 12variable; // SyntaxError: Invalid or unexpected token
