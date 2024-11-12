2d4b0b085360c105c299df88515983f23d49f9ac
Rahul Singh <thesincererahul@gmail.com>
2024-11-12 22:58:15 +0530

    Comments

---
+++ Javascript/3_Comments/comments.js
@@ -0,0 +1,4 @@
+/* comments are not execute, it is ignored by JS compiler/interpreter/JS engine */
+// Single line comment
+/* Multiline comment */
+// This will help to understand the lines of code to other programmers even after long time, it will to those programmers whose written this
