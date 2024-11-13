46415caeeda7f9eeabdc26916bf7a7c02aa59e4e
Rahul Singh <thesincererahul@gmail.com>
2024-11-13 11:07:26 +0530

    bitwise operators most important

---
+++ Javascript/2_Operators/bitwiseOperators.js
@@ -0,0 +1,16 @@
+const a = 5;
+const b = 6;
+
+console.log(a & b);
+
+console.log(a | b);
+
+console.log(a ^ b);
+
+console.log(5<<3);
+
+console.log(27>>2);
+
+console.log(!7); // false
+
+console.log(!10); // false
