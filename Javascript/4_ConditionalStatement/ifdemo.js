12dd805244959043d6bb5e8b51802fa68f87c2cd
Rahul Singh <thesincererahul@gmail.com>
2024-11-13 09:18:37 +0530

    Conditionals statements

---
+++ Javascript/4_ConditionalStatement/ifdemo.js
@@ -0,0 +1,12 @@
+console.log("Starting.....");
+// Hypothetical Situation
+let isUserSignedIn = true; // this denotes that user is signed in to the app
+let isPaidUser = true;
+
+if (isUserSignedIn && isPaidUser) {
+    console.log("Show the dashboard.");
+} else {
+    console.log("Show the homepage.");
+}
+
+console.log("End.....");
---
+++ Javascript/4_ConditionalStatement/nestedifelsedemo.js
@@ -0,0 +1,11 @@
+let isUserSubscribed = true;
+let userHasLionsGatePlay = false;
+let userHasSony = true;
+
+if(isUserSubscribed) {
+    if(userHasLionsGatePlay) {
+        console.log("Watch john wick");
+    } else if(userHasSony) {
+        console.log("Show the guardian");
+    }
+}
