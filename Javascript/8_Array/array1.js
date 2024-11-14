fcf96c239d612fb41143dfa61d1752e71651bb6e
Rahul Singh <thesincererahul@gmail.com>
2024-11-15 00:10:10 +0530

    update

--- Javascript/8_Array/array1.js
+++ Javascript/8_Array/array1.js
@@ -8,3 +8,13 @@ arr1[0] = 90; // modify
 
 arr1[2] = 's';
 console.log(arr1[1]); // undefined
+console.log(arr1['length']);
+
+const a = {
+    1 : "key as a symbol",
+    key : "Key as a string"
+};
+
+console.log(a["key"]);
+console.log(a.key);
+console.log(a[1]);
