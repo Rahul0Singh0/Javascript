3221db76c0966b3f66ccb6b267f4325a6d4e73ee
Rahul Singh <thesincererahul@gmail.com>
2024-11-14 16:29:15 +0530

    function completed

---
+++ Javascript/6_Functions/function9.js
@@ -0,0 +1,8 @@
+/**
+ * The new operator in JavaScript is used to create an instance of a user-defined object type or one of the built-in object types
+ *  that has a constructor function.
+ */
+const d = new Date();
+console.log(d);
+console.log(d.getDate());
+console.log(d.getDay());
