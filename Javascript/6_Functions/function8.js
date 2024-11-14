14b8133cad93577c1e43b3fe4c2ba3d74aa66db0
Rahul Singh <thesincererahul@gmail.com>
2024-11-14 15:56:56 +0530

    function8

---
+++ Javascript/6_Functions/function8.js
@@ -0,0 +1,9 @@
+console.log(Math.abs(-5)); // |x|
+console.log(Math.floor(8.7)); // 8
+console.log(Math.ceil(8.7)); // 9
+console.log(Math.round(8.1)); // 8
+console.log(Math.round(8.6)); // 9
+console.log(Math.min(2, 3)); //2
+console.log(Math.random()); // generates random no. between 0 and 10
+console.log(10, true, false, -10);
+
