11b8a138c388c74f4ab5b24138e0f9b4ba0256dc
Rahul Singh <thesincererahul@gmail.com>
2024-11-14 23:58:44 +0530

    Array() Constructor

---
+++ Javascript/8_Array/array2.js
@@ -0,0 +1,15 @@
+let arr = new Array(); // 0 size
+console.log(typeof(arr));
+console.log(arr.length);
+console.log(arr[4]);
+
+let arr1 = new Array(5); // array with size 5
+console.log(arr1.length);
+console.log(arr1);
+
+let arr2 = new Array(1,2); // [1, 2]
+console.log(arr2.length);
+console.log(arr2);
+
+let arr3 = new Array(5).fill(10); // filled with 10
+console.log(arr3);
