9b84ed9e5d066e679353a858bb99875592ddbf16
Rahul Singh <thesincererahul@gmail.com>
2024-11-14 23:45:17 +0530

    array intro

---
+++ Javascript/8_Array/array1.js
@@ -0,0 +1,10 @@
+let arr1 = []; // empty array
+let arr2 = ['rahul',1,null, true];
+console.log(arr1, arr2);
+console.log(typeof(arr1));
+
+console.log(arr1[0]); // access
+arr1[0] = 90; // modify
+
+arr1[2] = 's';
+console.log(arr1[1]); // undefined
