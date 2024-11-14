660a1b18f87ace43a9db92669ebeb5ad55c2157c
Rahul Singh <thesincererahul@gmail.com>
2024-11-14 16:44:35 +0530

    string introduced

---
+++ Javascript/7_String/string1.js
@@ -0,0 +1,12 @@
+let s1 = "hello world!";
+let s2 = "where";
+let s3 = "How are u?";
+let s4 = 'Raju said, "Where are u going"?';
+let s5 = `Rahul "Singh", 'Hello'`;
+console.log(s1, s2, s3, s4, s5);
+
+let x = 10;
+
+let str = `value of x is ${x}`;
+
+console.log(str);
