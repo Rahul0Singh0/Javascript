ba9ca764bf8ad1ee324b4a3c5f8412124b324ec7
Rahul Singh <thesincererahul@gmail.com>
2024-11-18 11:38:47 +0530

    example of scoping

---
+++ Javascript/09_Scopes/example4.js
@@ -0,0 +1,10 @@
+var student = "Sanket";
+console.log("Start"); // proved this line throw error at runtime(Phase2)
+console.log(bestFriend);
+function updateStudentInfo() {
+    // varr student = "Sarthak";
+    var student = "Sarthak";
+    bestFriend = "Tanmay";
+    console.log(student);
+}
+console.log(student);
