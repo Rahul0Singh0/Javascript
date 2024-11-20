13415049daa9ff133babd902c3d53ff7b50b2d10
Rahul Singh <thesincererahul@gmail.com>
2024-11-20 21:19:00 +0530

    examples to understands the use of reserved keywords

---
+++ 12_OOPS/example1.js
@@ -0,0 +1,19 @@
+const obj = {
+    this: "abc",
+    new: 'hello',
+    function() {
+        console.log(this);
+    },
+};
+// we can use all the reserved keywords as a property name
+// this is not good practice 
+// because you are doing to use reserved keyword as custom identifier
+console.log(obj);
+console.log(obj.this);
+console.log(obj.new);
+
+function constructor () {
+    console.log("We are using constructor keyword as function name");
+}
+
+constructor();
