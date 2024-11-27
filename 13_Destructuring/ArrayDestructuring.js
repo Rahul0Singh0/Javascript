cb06d206eacf00a52d46e30ac2d2641b948dd67f
Rahul Singh <thesincererahul@gmail.com>
2024-11-27 17:02:01 +0530

    Array Destructuring

---
+++ 13_Destructuring/ArrayDestructuring.js
@@ -0,0 +1,8 @@
+// const [ english, hindi, maths, science, sst ] = [ 100, 100, 998, 99, 100 ];
+// console.log(english);
+const [ english, hindi, maths ] = [ 100, 100, 998, 99, 100 ];
+console.log(maths);
+
+const x = [1, 2, 3];
+const y = [4, 5, 6, ...x];
+console.log(y);
