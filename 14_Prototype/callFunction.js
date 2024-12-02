bc4a3baed8c497486ffc87ca4059cce4a1e9fe46
Rahul Singh <thesincererahul@gmail.com>
2024-12-02 21:42:24 +0530

    call function

---
+++ 14_Prototype/callFunction.js
@@ -0,0 +1,16 @@
+// In arrow fun, this keyword resolve lexically whereas other than this keyword
+// resloved by callsite
+const obj = { 
+    firstName: "Sanket",
+    greet: () => {
+        // check in current scope, this is defined or not
+        // if not then check it in outer scope(global scope)
+        // in global scope it is defined but firstName is not defined in this keyword
+        console.log("hello, my name is", this.firstName);
+    }
+}
+// in most cases this keyword refers to the call site.
+obj.greet(); // callsite is obj
+
+const newObj = {firstName: 'Sarthak'};
+newObj.greet.call(newObj);
