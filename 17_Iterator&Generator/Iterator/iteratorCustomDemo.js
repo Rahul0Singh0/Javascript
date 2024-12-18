f0126a60a0e3b1540123c71a4d8c8501b7e70959
Rahul Singh <thesincererahul@gmail.com>
2024-12-18 13:07:23 +0530

    add 3 properties into custom iterator

--- 17_Iterator&Generator/Iterator/iteratorCustomDemo.js
+++ 17_Iterator&Generator/Iterator/iteratorCustomDemo.js
@@ -1,22 +1,22 @@
-function fetchNextElement(arr) {
-    let idx = 0; // closure concept used 
-    function next() {
-        // some logic
-        if(idx >= arr.length) {
-            return undefined;
-        }
-        const newElement = arr[idx];
-        idx++;
-        return newElement;
-    }
-    return next;
-}
+// function fetchNextElement(arr) {
+//     let idx = 0; // closure concept used 
+//     function next() {
+//         // some logic
+//         if(idx >= arr.length) {
+//             return undefined;
+//         }
+//         const newElement = arr[idx];
+//         idx++;
+//         return newElement;
+//     }
+//     return next;
+// }
 
-const autoFetcher = fetchNextElement([1, 2, 3, 4, 5]);
-console.log(autoFetcher());
-console.log(autoFetcher());
-for(let i = 0; i < 10000000000; i++) {}
-console.log(autoFetcher());
+// const autoFetcher = fetchNextElement([1, 2, 3, 4, 5]);
+// console.log(autoFetcher());
+// console.log(autoFetcher());
+// for(let i = 0; i < 10000000000; i++) {}
+// console.log(autoFetcher());
 
 // arr = [1, 2, 3, 4, 5]
 // fetchNextElement(arr) => 1
@@ -25,3 +25,26 @@ console.log(autoFetcher());
 // fetchNextElement(arr) => 4
 // fetchNextElement(arr) => 5
 // fetchNextElement(arr) => undefined
+
+function fetchNextElement(arr) {
+    let idx = 0; // closure concept used 
+    function next() {
+        // some logic
+        if(idx >= arr.length) {
+            return {value: undefined, done: true};
+        }
+        const newElement = arr[idx];
+        idx++;
+        return {value: newElement, done: false};
+    }
+    return {next};
+}
+
+const autoFetcher = fetchNextElement([1, 2, 3, 4, 5]);
+console.log(autoFetcher.next());
+console.log(autoFetcher.next());
+console.log(autoFetcher.next());
+console.log(autoFetcher.next());
+console.log(autoFetcher.next());
+console.log(autoFetcher.next());
+console.log(autoFetcher.next());
